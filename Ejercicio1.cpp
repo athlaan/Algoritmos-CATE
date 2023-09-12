b879fa853b3fa50e0a92183999575a8283436a3b
アトランゼラ <102254054+athlaan@users.noreply.github.com>
2023-09-12 10:15:34 -0300

    Ejercicios FOR 12/09/23

---
+++ Ejercicio1.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+	
+	int numero;
+	
+	printf("Ingrese el numero que desea mostrar desde 0: ");
+	scanf("%d", &numero);
+	
+	for(int i = 0; i <= numero; i++){
+		
+		printf("%d\n", i);
+		
+	}
+	
+	
+	
+}
