c33cc3589cb5d804d8f32978c2aa7c6c0824a5fa
アトランゼラ <102254054+athlaan@users.noreply.github.com>
2023-09-13 05:36:30 -0600

    Add files via upload

---
+++ Ejercicio3.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+	
+	int num;
+	
+	printf("Ingrese un numero: ");
+	scanf("%d", &num);
+	
+	for(int i = 0; i < num; i++){
+		
+		if(i % 2 == 0){
+			printf("%d", i);
+		}
+	}
+	
+}
